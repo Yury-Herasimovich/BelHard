3fa5751f6f2c5992b1f9daaed2ebcb84551b8e7f
HerrYury <yurasgerasimovich@gmail.com>
2025-03-31 17:52:55 +0300

    Tasks 3 and 4 finished. API Key is hidden

--- HW2/Flask_server.py
+++ HW2/Flask_server.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template
 import os
 import requests
 import random
+from HW2.config import Config
 
 BASE_DIR = os.path.dirname(__file__)
+API_KEY = Config.OWM_API_KEY
+# API_KEY = '23232775d430e5fe2ac9a9c2cbdb8410'
 
 app = Flask(__name__,
             static_folder=os.path.join(BASE_DIR, 'static') , 
             template_folder=os.path.join(BASE_DIR, 'templates'))
 
+
+BASE_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 @app.route("/")
 def index():
@@ -25,7 +30,7 @@ def duck_page():
    
 @app.route('/fox/')
 @app.route('/fox/<int:count>')
-def show_foxes(count=1):
+def fox_page(count=1):
     if count < 1 or count > 10:
         return "Ошибка: можно запросить только от 1 до 10 лис", 400
     
@@ -39,6 +44,44 @@ def show_foxes(count=1):
         })
     
     return render_template('fox.html', foxes=foxes)
+
+@app.route('/weather-minsk/')
+def weather_minsk_page():
+    return get_weather_data("Minsk")
+
+@app.route('/weather/<city>/')
+def weather_city_page(city):
+    return get_weather_data(city)
+
+def get_weather_data(city_name):
+    try:
+        params = {
+            'q': city_name,
+            'appid': API_KEY,
+            'units': 'metric'
+        }
+
+        response = requests.get(BASE_WEATHER_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        weather_data = {
+            'city': data['name'],
+            'temp': round(data['main']['temp']),
+            'feels_like': round(data['main']['feels_like']),
+            'description': data['weather'][0]['description'].capitalize(),
+            'humidity': data['main']['humidity'],
+            'wind': round(data['wind']['speed'])
+        }
+
+        return render_template('weather.html', weather=weather_data)
+        
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return render_template('weather_error.html', city=city_name)
+
+
 
 
 @app.errorhandler(404)
